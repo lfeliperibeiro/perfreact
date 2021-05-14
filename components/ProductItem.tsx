d035a53890f433ee65196a9d0d2d5ece6c80207b
Felipe Ribeiro <piporibeiro@gmail.com>
2021-05-14 20:58:42 -0300

    feat: add memo for product item

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -5,10 +7,20 @@ interface ProductItemProps {
     title: string;
   };
 }
-export function ProductItem({ product }: ProductItemProps) {
+export function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+// Faz uma comparação rasa
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    // Object.is faz uma comparação profunda
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
