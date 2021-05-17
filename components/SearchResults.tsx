808b22137dd2c41866f165427522974cea05b9ec
Felipe Ribeiro <piporibeiro@gmail.com>
2021-05-17 20:49:37 -0300

    feat: format price

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -2,20 +2,18 @@ import { ProductItem } from "./ProductItem";
 import {useMemo} from "react";
 
 interface SearchResultsProps {
+  totalPrice: number;
   results: Array<{
     id: number;
     price: number;
+    priceFormatted: string;
     title: string;
   }>;
   onAddToWishlist: (id: number) => void;
 }
 
-export function SearchResults({ results, onAddToWishlist }: SearchResultsProps) {
-  const totalPrice = useMemo(() => {
-    return results.reduce((total, product) => {
-      return total + product.price;
-    }, 0)
-  }, [results])
+export function SearchResults({ results, onAddToWishlist, totalPrice }: SearchResultsProps) {
+
   return (
     <div>
       <h2>{totalPrice}</h2>
