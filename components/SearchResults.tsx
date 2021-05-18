e6765b99e47a3e401110b3e3105f980303c32bed
Felipe Ribeiro <piporibeiro@gmail.com>
2021-05-18 20:00:14 -0300

    feat: create virtualized list

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,5 +1,5 @@
 import { ProductItem } from "./ProductItem";
-import {useMemo} from "react";
+import { List, ListRowRenderer } from 'react-virtualized'
 
 interface SearchResultsProps {
   totalPrice: number;
@@ -13,19 +13,36 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results, onAddToWishlist, totalPrice }: SearchResultsProps) {
-
+  const rowRenderer: ListRowRenderer = ({index, key, style}) => {
+      return (
+          <div key={key} style={style}>
+          <ProductItem
+            product={results[index]}
+            onAddToWishlist={onAddToWishlist}
+     />
+          </div>
+      )
+  }
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map((product) => {
-        return (
-            <ProductItem
-              key={product.id}
-              product={product}
-              onAddToWishlist={onAddToWishlist}
-            />
-        );
-      })}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+      {/*{results.map((product) => {*/}
+      {/*  return (*/}
+      {/*      <ProductItem*/}
+      {/*        key={product.id}*/}
+      {/*        product={product}*/}
+      {/*        onAddToWishlist={onAddToWishlist}*/}
+      {/*      />*/}
+      {/*  );*/}
+      {/*})}*/}
     </div>
   );
 }
